c00df9a46c3d17e2373133e8013560877e089b02
CMP_HoGoodwill <CMP_HoGoodwill@RM37-7>
2024-03-11 09:15:19 -0400

    Add project files.

---
+++ States and capital/Form1.Designer.vb
@@ -0,0 +1,103 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lstStates = New System.Windows.Forms.ListBox()
+        Me.lblCapital = New System.Windows.Forms.Label()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lstStates
+        '
+        Me.lstStates.FormattingEnabled = True
+        Me.lstStates.Items.AddRange(New Object() {"Idaho", "Marryland", "Ohio", "Rhode Island", "Virginia"})
+        Me.lstStates.Location = New System.Drawing.Point(12, 29)
+        Me.lstStates.Name = "lstStates"
+        Me.lstStates.Size = New System.Drawing.Size(120, 95)
+        Me.lstStates.TabIndex = 0
+        '
+        'lblCapital
+        '
+        Me.lblCapital.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.lblCapital.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblCapital.Location = New System.Drawing.Point(168, 29)
+        Me.lblCapital.Name = "lblCapital"
+        Me.lblCapital.Size = New System.Drawing.Size(146, 47)
+        Me.lblCapital.TabIndex = 1
+        '
+        'btnExit
+        '
+        Me.btnExit.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(215, 91)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(99, 31)
+        Me.btnExit.TabIndex = 2
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(168, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(62, 20)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Capital:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(60, 20)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "States:"
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSize = True
+        Me.ClientSize = New System.Drawing.Size(327, 134)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.lblCapital)
+        Me.Controls.Add(Me.lstStates)
+        Me.Name = "frmMain"
+        Me.Text = "H.Goodwill-States and Capitals"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lstStates As ListBox
+    Friend WithEvents lblCapital As Label
+    Friend WithEvents btnExit As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+End Class
---
+++ States and capital/Form1.vb
@@ -0,0 +1,47 @@
+﻿'Holly Goodwill
+'States and Capitals excerise 
+'3/11/2024
+
+Option Strict On
+Option Infer Off
+Option Explicit On
+
+Public Class frmMain
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub lblCapital_Click(sender As Object, e As EventArgs) Handles lblCapital.Click
+
+        If lstStates.SelectedIndex = 0 Then
+
+            lblCapital.Text = "Boise"
+
+        End If
+
+        If lstStates.SelectedIndex = 1 Then
+
+            lblCapital.Text = "Annapolis"
+
+        End If
+
+        If lstStates.SelectedIndex = 2 Then
+
+            lblCapital.Text = "Columbus"
+
+        End If
+
+        If lstStates.SelectedIndex = 3 Then
+
+            lblCapital.Text = "Rhode Island"
+
+        End If
+
+        If lstStates.SelectedIndex = 4 Then
+
+            lblCapital.Text = "Richmond"
+
+        End If
+    End Sub
+
+End Class
